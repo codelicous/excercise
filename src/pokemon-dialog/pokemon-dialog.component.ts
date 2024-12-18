fb96673f11d5344ccdf23eaae9bd31c4a4e08006
codelicous <ktomkt86@gmail.com>
2024-12-18 08:09:36 +0200

    added color themes adjusted to the types

--- src/pokemon-dialog/pokemon-dialog.component.ts
+++ src/pokemon-dialog/pokemon-dialog.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from '@angular/core';
+import {Component, HostBinding, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {ImageDisplayerComponent} from './image-displayer/image-displayer.component';
 
@@ -11,7 +11,28 @@ import {ImageDisplayerComponent} from './image-displayer/image-displayer.compone
   styleUrl: './pokemon-dialog.component.css'
 })
 export class PokemonDialogComponent {
+  @HostBinding('class') dialogClass = 'default';
+  getMainType(): string {
+    return  this.data?.types[0]?.type?.name;
+  }
+    setDialogClass(){
+      switch (this.data.types[0]){
+        case 'water':
+          this.dialogClass = 'blue';
+      }
+    }
     constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
-
+    console.log(this.getMainType());
+      switch (this.getMainType()) {
+        case 'grass':
+          this.dialogClass = 'green';
+          break;
+        case 'fire':
+          this.dialogClass = 'fire';
+          break;
+        case 'water':
+          this.dialogClass = 'blue';
+          break;
+      }
     }
 }
